931bd4aeb37e1bd48fb96c81f76a86ff952951c9
Leo <leonardo.trolese@studenti.unipd.it>
2025-05-21 14:56:58 +0200

    Conclusione test di unità sulla componente plugin

--- src/plugin/commands/QueryDatabase.py
+++ src/plugin/commands/QueryDatabase.py
@@ -5,6 +5,10 @@ from typing import List
 
 class QueryDatabase(Command):
     def __init__(self, manager: DBManager, embeddings: List[float]):
+        if not isinstance(manager, DBManager):
+            raise TypeError(f"Param manager must be a DBmanager object, not {type(manager)}")
+        if embeddings is None:
+            raise ValueError("Param embeddings cannot be None")
         self._DBmanager = manager
         self._embeddings = embeddings
 
